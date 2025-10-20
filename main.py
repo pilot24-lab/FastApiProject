37d30b21539bfb84856478ffbab91e03e4697215
Nikita <nickita.rfonline@yandex.ru>
2025-10-20 17:09:47 +0300

    Second lesson not full

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
-
+from pydantic import BaseModel
 app = FastAPI()
-@app.get('/')
-def root():
-    return "Hello, World!"
+books = [
+    {"id": 1, "title": "1984", "author": "George Orwell"},
+    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    {"id": 3, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"}
+]
+@app.get('/books', tags = ['Книги'], summary="Получить все книги", description="Возвращает список всех книг в библиотеке")
+def read_books():
+    return books
 
+@app.get('/books/{id}', tags = ['Книги'], summary="Получить книгу по ID", description="Возвращает информацию о книге по её уникальному идентификатору")
+def get_book(id: int):
+    for book in books:
+        if book['id'] == id:
+            return book
+    raise HTTPException(status_code=404, detail="Book not found")
+
+class NewBook(BaseModel):
+    title: str
+    author: str
+
+@app.post('/books')
+def create_book(new_book: NewBook):
+    book_id = len(books) + 1
+    book = {"id": book_id, "title": new_book.title, "author": new_book.author}
+    books.append(book)
+    return book
+    
 if __name__ == "__main__":
     uvicorn.run('main:app', reload=True)
